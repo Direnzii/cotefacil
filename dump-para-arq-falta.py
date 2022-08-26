29edea8cc8dbee79562c81e536f8c5347b689100
Direnzi <98608748+Direnzii@users.noreply.github.com>
2022-08-26 18:20:48 -0300

    Update dump-para-arq-falta.py

--- dump-para-arq-falta.py
+++ dump-para-arq-falta.py
@@ -1,36 +1,35 @@
 import sys
 
-def criar_arquivo(arquivo, novo_arquivo):
+
+def criar_arquivo(arquivo):
     with open(arquivo, 'r') as arquivo:
         contador = 0
-        saida = ''
-        saida += f'1;<CNPJ DO CLIENTE>;3.3\n'
+        saida = '1;<CNPJ_CLIENTE>;3.3\n'
+        #try:
         for linha in arquivo:
-            separado = linha.split()
-            ean = separado[5]
-            descricao_listas = separado[6:2000]
-            i = 0
-            descricao_do_produto = ''
-            while i < len(ean):
-                try:
-                    descricao_do_produto += descricao_listas[i] + ' '
-                    i += 1
-                    contador += 1
-                except:
-                    descricao = descricao_do_produto
-                    saida += f'2;{ean};1;{ean};{descricao};.\n'
-                    break
-    saida += f'9;{contador}'
-    with open(f'{novo_arquivo}', 'w') as arquivo_falta:
-        arquivo_falta.write(saida)
+            try:
+                codigo_produto = linha.split()[0]
+                ean = linha.split()[1]
+            except:
+                continue
+            descricao_lista = linha.split()[2::]
+            descricao = ' '.join(descricao_lista)
+            contador += 1
+            saida += f'2;{ean};1;{codigo_produto};{descricao};.;0\n'
+
+        saida += f'9;{contador}'
+        with open(f'novo-arquivo-de-falta-base.txt', 'w') as arquivo:
+            arquivo.write(saida)
+
+#criar_arquivo('dump-base-martins.txt')  --- testar
 
 def main():
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 2:
         arquivo = (str(sys.argv[1]))
-        novo_arquivo = (str(sys.argv[2]))
-        criar_arquivo(arquivo=arquivo, novo_arquivo=novo_arquivo)
+        criar_arquivo(arquivo=arquivo)
     else:
-        print(f'\nVOCÊ PRECISA PASSAR 2 ARGUMENTOS:\n\nEx do comando: \n\npython {sys.argv[0]} <NOME DO ARQUIVO .txt> <NOME PARA O ARQUIVO DE FALTA .TXT>\n\n')
-
+        arquivo = 'ol-dump.txt'
+        criar_arquivo(arquivo=arquivo)
+        print(f'\nVOCÊ PRECISA PASSAR 1 ARGUMENTOS:\n\nEx do comando: \n\npython {sys.argv[0]} <NOME DO ARQUIVO .txt>\n\n')
 if __name__ == "__main__":
     main()
